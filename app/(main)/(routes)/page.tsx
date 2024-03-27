ff0817af38971f327373dac612b85ab4b1a9c05e
Yvan jr Blanchette <yvanblanchette@gmail.com>
2024-03-27 08:17:42 -0400

    Shadcn initialization and folder structure organisation

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+	return (
+		<div>
+			<p className="text-3xl font-bold text-indigo-600">Hello Discord Clone</p>
+			<b>THIS IS A PROTECTED ROUTE!!!</b>
+		</div>
+	);
+}
